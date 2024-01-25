41300809e68a4cd901e15faf1019c6344a39bb0a
regisatl <regisattolou21@outlook.fr>
2024-01-25 11:41:49 +0100

    creation de l'interface de convertion

--- app.py
+++ app.py
@@ -6,3 +6,20 @@ class ImageToPDFConverter:
       def __init__(self, root):
             self.root = root
             self.image_paths = []
+            self.output_pdf_name = tk.StringVar()
+            self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
+            
+            self.initialize_ui()            
+      
+      def initialize_ui(self):
+            title_label = tk.Label(self.root, text="Image to PDF Converter", font=("Helvetica", 16, "bold"))
+            title_label.pack(pady=10)
+
+def main():
+      root = tk.Tk()
+      root.title("Image to PDF")
+      root.geometry("400x600")
+      root.mainloop()
+
+if __name__ == "__main__":
+      main()
