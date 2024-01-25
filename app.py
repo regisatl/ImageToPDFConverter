bd20d08a7d6d60860ba7c376d9649f9211ef3916
regisatl <regisattolou21@outlook.fr>
2024-01-25 11:31:46 +0100

    First initialise git

---
+++ app.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+class ImageToPDFConverter:
+      def __init__(self, root):
+            self.root = root
+            self.image_paths = []
